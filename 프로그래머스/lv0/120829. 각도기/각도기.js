b34f13e91297cd0cf4ab4cbffb01b71194292309
Seungmi Lee <seungmilee.dev@gmail.com>
2023-08-28 15:32:50 +0100

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,12 @@
+function solution(angle) {
+    var answer = 0;
+    if(angle > 0 && angle < 90) {
+        return answer = 1;
+    } else if(angle === 90) {
+        return answer = 2;
+    } else if(angle > 90 && angle < 180) {
+        return answer = 3;
+    } else if(angle === 180) {
+        return answer = 4;
+    }
+}
