10a853ef579a308203f9ed27f679a1d267ad2f44
Seungmi Lee <seungmilee.dev@gmail.com>
2023-08-29 16:20:35 +0100

    [level 0] Title: 짝수는 싫어요, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = [];
+    for(let i = 0; i <= n; i++) {
+        if(i % 2 == 1) {
+            answer.push(i);
+            answer.sort((a, b) => a - b);
+        }
+    }
+    return answer;
+}
